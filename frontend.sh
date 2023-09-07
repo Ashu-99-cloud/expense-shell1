ce4007684992308de8aa115fa8558d704ff501ce
ashu-99-cloud <ashishujawane1999@gmail.com>
2023-09-07 21:40:57 +0530

    adding echo comments to frontend.sh

--- frontend.sh
+++ frontend.sh
@@ -1,20 +1,20 @@
 echo Installing Nginx
-dnf install nginx -y >/tmp/expense.log
+dnf install nginx -y >>/tmp/expense.log
 
 echo placing expense config file in Nginx
-cp expense.conf /etc/nginx/default.d/expense.conf  >/tmp/expense.log
+cp expense.conf /etc/nginx/default.d/expense.conf  >>/tmp/expense.log
 
 echo removing old Nginx content
-rm -rf /usr/share/nginx/html/*  >/tmp/expense.log
+rm -rf /usr/share/nginx/html/*  >>/tmp/expense.log
 
 echo download frontend code
-curl -s -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip  >/tmp/expense.log
+curl -s -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip  >>/tmp/expense.log
 
-cd /usr/share/nginx/html  >/tmp/expense.log
+cd /usr/share/nginx/html  >>/tmp/expense.log
 
 echo extracting frontend code
-unzip /tmp/frontend.zip  >/tmp/expense.log
+unzip /tmp/frontend.zip  >>/tmp/expense.log
 
 echo starting Nginx services
-systemctl enable  nginx >/tmp/expense.log
-systemctl restart nginx  >/tmp/expense.log
+systemctl enable  nginx >>/tmp/expense.log
+systemctl restart nginx  >>/tmp/expense.log
