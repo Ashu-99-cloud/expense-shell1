69fe9b7ec02b05888fc095e788b480de5310c99f
ashu-99-cloud <ashishujawane1999@gmail.com>
2023-09-09 13:15:34 +0530

    remove && in backend.sh and added echo $? for checking status

--- backend.sh
+++ backend.sh
@@ -2,35 +2,44 @@ source common.sh
 component=backend
 
 echo Install NodeJS repos for backend.sh
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash &&>>$log_file
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$log_file
+echo $?
 
 echo Install NodeJS
 dnf install nodejs -y &>>$log_file
+echo $?
 
 echo Copy backend service file
 cp backend.service /etc/systemd/system/backend.service &>>$log_file
+echo $?
 
 echo add application user
 useradd expense &>>$log_file
+echo $?
 
 echo clean app content
 # sudo rm -rf expense-shell/
 rm -rf /app &>>$log_file
+echo $?
 mkdir /app
 cd /app
 
 download_and_extract
 
 echo Download dependencies
-npm install &&>>$log_file
+npm install &>>$log_file
+echo $?
 
 echo start backend service
 systemctl daemon-reload &>>$log_file
 systemctl enable backend &>>$log_file
 systemctl start backend &>>$log_file
+echo $?
 
 echo install MySQL client
 dnf install mysql -y &>>$log_file
+echo $?
 
 echo Load the schema
 mysql -h mysql.ashudevops.online -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$log_file
+echo $?
