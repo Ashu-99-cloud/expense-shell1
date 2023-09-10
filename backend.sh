f2debe8fd1d8a41befd72739985c49ea55d28373
ashu-99-cloud <ashishujawane1999@gmail.com>
2023-09-10 11:23:50 +0530

    If else condition is added to check success and failure

--- backend.sh
+++ backend.sh
@@ -3,24 +3,45 @@ component=backend
 
 echo Install NodeJS repos for backend.sh
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$log_file
-echo $?
+if [ $? -eq 0 ]; then
+  echo success
+else
+  echo failed
+fi
 
 echo Install NodeJS
 dnf install nodejs -y &>>$log_file
-echo $?
+if [ $? -eq 0 ]; then
+  echo success
+else
+  echo failed
+fi
 
 echo Copy backend service file
 cp backend.service /etc/systemd/system/backend.service &>>$log_file
-echo $?
+if [ $? -eq 0 ]; then
+  echo success
+else
+  echo failed
+fi
 
 echo add application user
 useradd expense &>>$log_file
-echo $?
+if [ $? -eq 0 ]; then
+  echo success
+else
+  echo failed
+fi
 
 echo clean app content
 # sudo rm -rf expense-shell/
 rm -rf /app &>>$log_file
-echo $?
+if [ $? -eq 0 ]; then
+  echo success
+else
+  echo failed
+fi
+
 mkdir /app
 cd /app
 
@@ -28,18 +49,34 @@ download_and_extract
 
 echo Download dependencies
 npm install &>>$log_file
-echo $?
+if [ $? -eq 0 ]; then
+  echo success
+else
+  echo failed
+fi
 
 echo start backend service
 systemctl daemon-reload &>>$log_file
 systemctl enable backend &>>$log_file
 systemctl start backend &>>$log_file
-echo $?
+if [ $? -eq 0 ]; then
+  echo success
+else
+  echo failed
+fi
 
 echo install MySQL client
 dnf install mysql -y &>>$log_file
-echo $?
+if [ $? -eq 0 ]; then
+  echo success
+else
+  echo failed
+fi
 
 echo Load the schema
 mysql -h mysql.ashudevops.online -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$log_file
-echo $?
+if [ $? -eq 0 ]; then
+  echo success
+else
+  echo failed
+fi
--- frontend.sh
+++ frontend.sh
@@ -3,12 +3,27 @@ component=frontend
 
 echo Installing Nginx
 dnf install nginx -y &>>$log_file
+if [ $? -eq 0 ]; then
+  echo success
+else
+  echo failed
+fi
 
 echo placing expense config file in Nginx
 cp expense.conf /etc/nginx/default.d/expense.conf  &>>$log_file
+if [ $? -eq 0 ]; then
+  echo success
+else
+  echo failed
+fi
 
 echo removing old Nginx content
 rm -rf /usr/share/nginx/html/*  &>>$log_file
+if [ $? -eq 0 ]; then
+  echo success
+else
+  echo failed
+fi
 
 cd /usr/share/nginx/html
 
@@ -17,3 +32,8 @@ download_and_extract
 echo starting Nginx services
 systemctl enable  nginx &>>$log_file
 systemctl restart nginx  &>>$log_file
+if [ $? -eq 0 ]; then
+  echo success
+else
+  echo failed
+fi
